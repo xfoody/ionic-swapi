5adde03d0f31dbd0ed42befaf7b26a4dd22304d3
Fuedti Xayyavong <35578738+xfoody@users.noreply.github.com>
2019-05-06 19:11:48 -0500

    All planets recursively

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { merge } from 'rxjs';
+import { merge, race, empty } from 'rxjs';
+import { expand } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,15 @@ export class SwapiService {
 
 
   getPlanets() {
-    const p1 = this.httpSvc.get("https:/swapi.co/api/planets");
-    const p2 = this.httpSvc.get("https:/swapi.co/api/planets/?page=2");
+    // const p1 = this.httpSvc.get("https:/swapi.co/api/planets");
+    // const p2 = this.httpSvc.get("https:/swapi.co/api/planets/?page=2");
 
-    return merge(p1, p2);
+    // return merge(p1, p2);
 
+    return this.httpSvc.get("https:/swapi.co/api/planets").pipe
+    (expand(data =>
+        (<any>data).next ? this.httpSvc.get((<any>data).next) : empty()
+      )
+    );
   }
 }
